ac3b6452b806c1724733154a4239db3227106cca
camilo-camargo <camilocamargo49@gmail.com>
2023-04-14 10:28:24 -0500

    Add project structure

---
+++ api/src/test/java/com/learn/sprintusers/SprintusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sprintusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import springLogo from './assets/spring.svg';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://spring.io/" target="_blank">
+          <img src={springLogo} alt="Spring Boot Logo"  className="logo"/>
+        </a>
+      </div>
+      <h1>Vite + React + Spring Boot</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
