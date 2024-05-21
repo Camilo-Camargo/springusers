e81c4ffb9ab284645631bf241c6dd2f441881468
camilo-camargo <camilocamargo49@gmail.com>
2024-05-20 21:17:09 -0500

    feat: add cors to every location

--- api/src/main/java/com/learn/springusers/SpringUsersApplication.java
+++ api/src/main/java/com/learn/springusers/SpringUsersApplication.java
@@ -2,7 +2,11 @@ package com.learn.springusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringUsersApplication {
@@ -14,5 +18,18 @@ public class SpringUsersApplication {
 	@RequestMapping("/")
 	public String forward() {
 		return "";
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*"); // Allow all origins
+		config.addAllowedHeader("*"); // Allow all headers
+		config.addAllowedMethod("*"); // Allow all methods
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		return new CorsFilter(source);
 	}
 }
